9ea0467161652cdfea415a45ea914369e6840b74
rocket-pig <prolefeed@gmail.com>
2019-05-23 12:11:56 -0700

    read user/pass from .cookie in config.php

--- libs/config.php
+++ libs/config.php
@@ -1,14 +1,20 @@
 <?php
+        // where is your .navcoin4/.cookie? 
+        $navcookie = ".cookie";
 
-	$wallets = array();
 
-	$wallets['NavCoin'] = array(
-		"user" => "username",  
-		"pass" => "stakebox",      
-		"host" => "localhost",     
-		"port" => 44444,
-		"protocol" => "http",
-		"ticker" => "NAV"
-	);
+        // get user/pass from .navcoin4/.cookie:
+        $user =  explode(":",file_get_contents($navcookie))[0];
+        $pass =  urlencode(explode(":",file_get_contents($navdir.".cookie"))[1]);
 
+        $wallets = array();
+        $wallets['NavCoin'] = array(
+                "user" => $user,
+                "pass" => $pass,
+                "host" => "localhost",
+                "port" => 44444,
+                "protocol" => "http",
+                "ticker" => "NAV"
+        );
 ?>
+
